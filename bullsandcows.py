5f4d9c694f009a0e2ddcb4b33ba49028a6b6c5f7
Ondrej Zapletal <ondrej.matej.zapletal@gmail.com>
2017-02-12 17:29:44 +0100

    order of functions changed

--- bullsandcows.py
+++ bullsandcows.py
@@ -12,29 +12,16 @@ def generate_number():
     return unique_number
 
 
-def check_number(user_input, unique_number, attempts):
-    bulls = 0
-    cows = 0
-    for index, digit in enumerate(user_input):
-        if digit == unique_number[index]:
-            bulls += 1
-        else:
-            if digit in unique_number:
-                cows += 1
-    if bulls == 4:
-        print("Congratulation you win! It took you %s tries." % attempts)
-        return False
+def process_user_input(unique_number, attempts):
+    user_input = input(">>> ")
+    if is_valid(user_input):
+        return check_number(user_input, unique_number, attempts)
     else:
-        print("%s bulls, %s cows" % (bulls, cows))
-        return True
-
-
-def is_number(user_input):
-    try:
-        int(user_input)
-        return True
-    except ValueError:
-        return False
+        if user_input == 'q':
+            print("Exiting!")
+            return False
+        else:
+            return True
 
 
 def is_valid(user_input):
@@ -53,6 +40,14 @@ def is_valid(user_input):
         return False
 
 
+def is_number(user_input):
+    try:
+        int(user_input)
+        return True
+    except ValueError:
+        return False
+
+
 def is_correct_length(user_input):
     return len(user_input) == 4
 
@@ -64,16 +59,21 @@ def is_non_repeting(user_input):
     return True
 
 
-def process_user_input(unique_number, attempts):
-    user_input = input(">>> ")
-    if is_valid(user_input):
-        return check_number(user_input, unique_number, attempts)
-    else:
-        if user_input == 'q':
-            print("Exiting!")
-            return False
+def check_number(user_input, unique_number, attempts):
+    bulls = 0
+    cows = 0
+    for index, digit in enumerate(user_input):
+        if digit == unique_number[index]:
+            bulls += 1
         else:
-            return True
+            if digit in unique_number:
+                cows += 1
+    if bulls == 4:
+        print("Congratulation you win! It took you %s tries." % attempts)
+        return False
+    else:
+        print("%s bulls, %s cows" % (bulls, cows))
+        return True
 
 
 print("Hi there!")
